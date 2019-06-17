00533c10c5f0b72c34d68f5e0d4e6e85996d7888
Alex Leitch <alex.leitch@gmail.com>
2019-06-17 00:22:04 -0400

    panel window generates successfully

--- air-conditioners.js
+++ air-conditioners.js
@@ -17,8 +17,8 @@ function setup() {
 
   stroke(0); // Set line drawing color to white
 
-  frameRate(3);
-  noLoop();
+  frameRate(2);
+  // noLoop();
   // TODO: Implement a pause button in the drawing function
 }
 
@@ -34,104 +34,63 @@ function draw() {
 }
 
 function basicStory(o = buildingOrigin, size = storyBlock) {
-  console.log('add a story', o, size);
+  // console.log('add a story', o, size);
 
   const baseBlock = rect(...o, ...size);
-  // const cols = 4;
 
-  // for (i = 0; i < cols + 1; i += 1) {
-  //   const x = margin + (buildingWidth / cols * i);
-  //   column(x)
-  // }
+  // symmetricSeries(getRandomIntInclusive(1, 7), airConditioner, airConditionerNumbers );
+  // symmetricSeries(7);
 
-  cols(getRandomIntInclusive(1, 7));
+  windowArch();
 
   let c = color(255, 255, 255);
   fill(c);
   baseBlock;
 }
 
-function cols(quantity = 5, origin = margin + buildingWidth / 2) {
-  // columns are symmetric in pairs
-  // columns can come in odd numbers, in which case they are symmetric around the middle column
-  console.log('cols', quantity, origin);
+function windowArch() {
+  // draw an arch with equivalent start and end x/y
+  const initialWidth = 120;
+  const initialHeight = 80;
+  
+  noFill();
+  stroke(0,0, 5);
+  const startEndY = 5;
+  const height = startEndY * 2;
+  
+  const x1 = 5;
+  const x2 = x1 + height/2;
+
+  curve(x1, startEndY, x1, height, x2, height, x2, startEndY);
+}
+
+function symmetricSeries(
+  quantity = 5,
+  element,
+  style,
+  rgb = color(241, 170, 100)) {
+  originX = margin + buildingWidth / 2,
+    // columns are symmetric in pairs
+    // columns can come in odd numbers, in which case they are symmetric around the middle column
+
+    fill(rgb);
+
   const pairs = (!isEven(quantity)) ?
     (quantity - 1) / 2 :
     quantity / 2;
 
   if (!isEven(quantity)) {
-    // odd number - draw columns from the inside out with 1 at middle
-    column(origin);
+    // odd number - draw symmetric from the inside out with 1 at middle
+    element(style, originX);
   }
 
   for (let i = 0; i < pairs; i += 1) {
-    console.log('make pairs', buildingWidth / quantity, pairs);
-    const originDistance = (buildingWidth / quantity) * (i + 1);
-    pair(originDistance, origin);
+    const originDistance = (buildingWidth / quantity);
+    const x1 = originX + originDistance * (i + 1);
+    const x2 = originX - originDistance * (i + 1);
+    element(style, x1);
+    element(style, x2);
   }
-
-}
-
-function pair(x, origin, footWidth, middleWidth, capitalWidth) {
-  // Within a pair, things can't be random - things are pre-set in a pair.
-  const capital = () => { };
-
-  const foot = () => {
-    // const footW = w + getRandomIntInclusive(0, 15);
-    const footW = w + 6;
-
-    rect(midline - footW / 2, y + storyHeight - h / 6, footW / 2, h / 6);
-    rect(midline, y + storyHeight - h / 6, footW / 2, h / 6);
-  };
-
-  const middle = () => {
-    /* MIDDLE BIT */
-    let c = color(255, 204, 0);
-    fill(c);
-
-    // left
-    rect(midline - w / 2, y, w / 2, h);
-
-    //right
-    rect(midline, y, w / 2, h);
-
-    // symmetry line
-    c = color(155, 0, 0);
-    fill(c);
-    rect(midline, y, 2, h);
-  };
-
-  column(origin + x);
-  column(origin - x);
-}
-
-function column(x = buildingOrigin[0], y = buildingOrigin[1], w = buildingWidth / 12, h = storyHeight) {
-  // a column starts at x and has enough blocks to fill to height
-  // each story has at least one but up to three "unique" column styles
-  // columns on the inside are thinner than those on the outside
-  // columns may have capitals on them
-  // columns may have feet on them
-  // a column body can be a spiral or a set of blocks or a decoration
-
-  // colour
-
-  // columns are symmetric
-  const midline = x;
-
-  /* CAPITAL */
-
-
-
-
-  /*
-    FOOT
-    - can be wider than the column
-    - y = 0 to start
-    - w = column width + (between 0-5 x 2) (symmetric)
-    - x = x - width/2
-  */
-
-
 }
 
 function range(num) {
--- columns.js
+++ columns.js
@@ -1,15 +1,16 @@
 let y = 100; // animated line
 
-const canvas = [960, 600]
-const stories = 4;
+const canvas = [960, 940]
+const stories = 8;
 
 const buildingWidth = canvas[0] / 2;
-const storyHeight = canvas[1] / stories;
+const storyHeight = (canvas[1] - 40) / stories;
+// const colWidth = 
 const storyBlock = [buildingWidth, storyHeight];
 
 const margin = (canvas[0] - buildingWidth) / 2; // center the building
 
-const buildingOrigin = [margin, canvas[1] - (storyHeight + 10)];
+const buildingOrigin = [margin, canvas[1] - (storyHeight - 20)];
 
 
 function setup() {
@@ -18,103 +19,173 @@ function setup() {
   stroke(0); // Set line drawing color to white
 
   frameRate(2);
-  // noLoop();
+  noLoop();
   // TODO: Implement a pause button in the drawing function
 }
 
 function draw() {
   background(255); // Set the background to black
-  y = y - 1;
-  if (y < 0) {
-    y = height;
-  }
-  line(0, y, width, y);
+  // onePaneWindow(150);
+  panelPane(150);
+  // for (let i = 0; i < stories; i += 1) {
+  //   const y = i > 0 ?
+  //     buildingOrigin[1] - (storyHeight * i + 2) :
+  //     buildingOrigin[1];
 
-  basicStory();
+  //   basicStory([margin, y], storyBlock, i + 1);
+  // }
 }
 
-function basicStory(o = buildingOrigin, size = storyBlock) {
-  // console.log('add a story', o, size);
-
-  const baseBlock = rect(...o, ...size);
-
-  symmetricSeries(getRandomIntInclusive(1, 7));
+function basicStory(o = buildingOrigin, size = storyBlock, scaleWeight) {
+  symmetricSeries(
+    [
+      margin + buildingWidth / 2,
+      o[1] - (10 * scaleWeight)
+    ],
+    getRandomIntInclusive(1, 7)
+  );
   // symmetricSeries(7);
 
   let c = color(255, 255, 255);
   fill(c);
-  baseBlock;
+  stroke(0, 0, 0);
+  // baseBlock;
 }
 
-function symmetricSeries(quantity = 5, originX = margin + buildingWidth / 2, style, rgb) {
-  // columns are symmetric in pairs
-  // columns can come in odd numbers, in which case they are symmetric around the middle column
+function panelPane(w, x = 10, y = 10, cols = 3, rows =3) {
+  // let pane = [[0, 0]];
+  noFill();
+  // frame 
+  let frame = basicGoldenRectangle(w);
+  const {x: x1, y: y1, w: w1, h: h1} = frame.outer;
+  const {x: x2, y: y2, w: w2, h: h2} = frame.inner;
+  rect(x1, y1, w1, h1);
+  rect(x2, y2, w2, h2);
 
-  const columnStyle = style || genColumnNumbers(buildingWidth / 5);
+  // inject panels
+  let numbers = basicGoldenRectangle(w/cols);
+  for (let i = 0; i < cols; i += 1) {
+    for (let j = 0; j < rows; j += 1) {
+      let posX = x + ((numbers.outer.w) * i);
+      let posY = y + ((numbers.outer.h) * j);
 
-  let c = rgb || color(241, 170, 100);
-  fill(c);
-
-  const pairs = (!isEven(quantity)) ?
-  (quantity - 1) / 2 :
-  quantity / 2;
-
-  if(!isEven(quantity)) {
-    // odd number - draw columns from the inside out with 1 at middle
-   column(columnStyle, originX);
+      numbers = basicGoldenRectangle(w / cols, posX, posY);
+      const {x: x1, y: y1, w: w1, h: h1} = numbers.outer;
+      const {x: x2, y: y2, w: w2, h: h2} = numbers.inner;
+      rect(x1, y1, w1, h1);
+      rect(x2, y2, w2, h2);
+      // pane[i][j] = numbers;
+    }
   }
 
-  for (let i = 0; i < pairs; i += 1) {
-    const originDistance = (buildingWidth / quantity);
-    console.log('x, originDistance', originX, originDistance);
-    const x1 = originX + originDistance * (i + 1);
-    const x2 = originX - originDistance * (i + 1);
-
-    console.log('x1, x2', x1, x2);
-
-    column(columnStyle, x1);
-    column(columnStyle, x2);
-  }
-}
-
-function column(n, x) {
-  // console.log('draw a column', n, x);
   
+  // return information about pane?
+  // return {
+  //   topL: [pane[0][0].outer.x, pane[0][0].outer.y],
+  //   bottomR: [pane[3][2].outer.x, pane[0][0].outer.y]
+  // }
+}
+
+function onePaneWindow(w, x, y) {
+  const numbers = basicGoldenRectangle(w, x, y);
+
+  rect(...numbers.outer);
+  rect(...numbers.inner);
+}
+
+function basicGoldenRectangle(w, x = 10, y = 10) {
+  const bits = goldenRatio(w);
+  const innerPaneScale = 0.95;
+  const paneDiff = (1 - innerPaneScale) / 2;
+  console.log('paneDiff', paneDiff);
+  
+  const outer = {x, y, w: bits.a, h: bits.c };
+  const inner = {
+    x: x + bits.a/2 * paneDiff,
+    y: y + bits.c/2 * paneDiff,
+    w: bits.a * innerPaneScale,
+    h: bits.c * innerPaneScale
+  };
+
+  return {
+    outer, inner
+  }
+}
+
+function column(n, x, y) {
+  // console.log('draw a column', n, x);
+
   // middle
   let c = color(241, 170, 100);
   fill(c);
-  rect(x - n.middleWidth / 2, n.y, n.middleWidth, n.middleHeight);
+  rect(x - n.middleWidth / 2, y, n.middleWidth, n.middleHeight);
 
   // capital
   c = color(63, 191, 191);
   fill(c);
   // rect(x - n.footWidth / 2, n.y + n.middleHeight - (n.footHeight * 2), n.footWidth, n.footHeight);
-  rect(x - n.capWidth / 2, n.y, n.capWidth, n.capHeight);
+  rect(x - n.capWidth / 2, y, n.capWidth, n.capHeight);
 
   // foot
   c = color(191, 63, 127);
   fill(c);
-  rect(x - n.footWidth / 2, n.y + n.middleHeight - n.footHeight, n.footWidth, n.footHeight);
+  rect(x - n.footWidth / 2, y + n.middleHeight - n.footHeight, n.footWidth, n.footHeight);
 }
 
 function genColumnNumbers(width) {
+  // basic would be 32 id a building is 4x, so get number of stories and multiply
+  // these should be skinnier and proportionate to a building story, which is between 10 and 14 ft.
+  const baseMin = 32 / stories;
+  const baseMax = 24 / stories;
+
   const middleWidth = getRandomIntInclusive(buildingWidth / 32, buildingWidth / 24);
-  
+
   const capHeight = getRandomIntInclusive(middleWidth * 0.25, middleWidth * 3.14);
-  const capWidth = getRandomIntInclusive(middleWidth, middleWidth * 1.55);
-  
+  const capWidth = getRandomIntInclusive(middleWidth * 1.1, middleWidth * 2.09);
+
   const footHeight = getRandomIntInclusive(middleWidth * 0.25, middleWidth * 3.14);
-  const footWidth = getRandomIntInclusive(middleWidth * 1.1, capWidth);
+  const footWidth = getRandomIntInclusive(capWidth * 0.78, capWidth * 1.09);
 
 
   return {
-    y: buildingOrigin[1],
     middleWidth,
     middleHeight: storyHeight,
     capHeight,
     capWidth,
     footHeight,
     footWidth
+  }
+}
+
+function symmetricSeries(
+  origin = [
+    margin + buildingWidth / 2,
+    buildingOrigin[1]
+  ],
+  quantity = 5,
+  element = column,
+  style = genColumnNumbers(buildingWidth / 5),
+  rgb = color(241, 170, 100)) {
+  fill(rgb);
+
+  // columns are symmetric in pairs
+  // columns can come in odd numbers, in which case they are symmetric around the middle column
+
+  const pairs = (!isEven(quantity)) ?
+    (quantity - 1) / 2 :
+    quantity / 2;
+
+  if (!isEven(quantity)) {
+    // odd number - draw symmetric from the inside out with 1 at middle
+    element(style, ...origin);
+  }
+
+  for (let i = 0; i < pairs; i += 1) {
+    const originDistance = (buildingWidth / quantity);
+    const x1 = origin[0] + originDistance * (i + 1);
+    const x2 = origin[0] - originDistance * (i + 1);
+    element(style, x1, origin[1]);
+    element(style, x2, origin[1]);
   }
 }
 
@@ -135,3 +206,11 @@ function getRandomIntInclusive(min, max) {
 function isEven(someNumber) {
   return (someNumber % 2 == 0) ? true : false;
 };
+
+function goldenRatio(number) {
+  return {
+    a: number,
+    b: number * 0.618,
+    c: number * 1.618
+  }
+}
