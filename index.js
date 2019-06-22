491b1677f871a093029359dbdd6462596002fb8b
Alex Leitch <alex.leitch@gmail.com>
2019-06-22 19:13:51 -0400

    added fire escape generation

--- index.js
+++ index.js
@@ -51,14 +51,21 @@ function basicStory(o = buildingOrigin, size = storyBlock, scaleWeight) {
   // things should be symmetric
   // they can also be multiply-defined
 
-  symmetricWindowSeries(
-    getRandomIntInclusive(1, 3),
-    // 3,
-    panelStyles[window],
-    getRandomIntInclusive(32, 48),
-    [margin + buildingWidth / 2,
-    o[1] - (10 * scaleWeight)]
-  );
+  for (let i = 0; i < 2; i += 1) {
+    fireEscapeLayer(
+      buildingWidth / 2,
+      storyHeight
+    );
+  }
+  // rotate(-60);
+  // symmetricWindowSeries(
+  //   getRandomIntInclusive(1, 3),
+  //   // 3,
+  //   panelStyles[window],
+  //   getRandomIntInclusive(32, 48),
+  //   [margin + buildingWidth / 2,
+  //   o[1] - (10 * scaleWeight)]
+  // );
 
   // center line
   line(margin + buildingWidth / 2, o[1], margin + buildingWidth / 2, storyHeight);
@@ -66,4 +73,52 @@ function basicStory(o = buildingOrigin, size = storyBlock, scaleWeight) {
   fill(c);
   stroke(0, 0, 0);
   // baseBlock;
+}
+
+function fireEscapeLayer(w = buildingWidth / 2, h = storyHeight, x = buildingWidth / 2, y = storyHeight * 2.5) {
+  stroke(0, 0, 0);
+  noFill();
+  const railStart = x + w / 5;
+  const railEnd = w + x - w / 5;
+  line(railStart, y, railEnd, h)
+
+  stroke(0, 124, 124);
+  line(railStart + 10, y, railEnd, h + 10)
+
+  stroke(0, 124, 0);
+  line(railStart, y - 15, railEnd, h - 15) // center line
+  stroke(0, 0, 0);
+
+  line(railStart, y - 22, railEnd, h - 22)
+
+  fill(255,255,255);
+  circle(railStart, y - 20, 5)
+  noFill();
+  circle(railStart, y - 20, 9)
+  fill(255,255,255);
+  circle(railEnd, h - 18, 9)
+  circle(railEnd, h - 18, 5)
+  noFill();
+  
+  rect(x, y - h / 3, w, h / 3);
+
+  const railSupports = 9;
+  for (let i = 1; i <= railSupports; i += 1) {
+    const rise = (x) => y + 15 - ((h + 56) / railSupports) * x;
+    const run = railStart + ((railEnd - railStart) / railSupports * i);
+    line(run, rise(i), run, rise(i) - h / 3)
+    rect(run - 7, rise(i) - 12, 7, 3);
+  }
+
+  const numberOfSupports = 18;
+  for (let i = 0; i < numberOfSupports; i += 1) {
+    line(x + (w / numberOfSupports * i), y, x + (w / numberOfSupports * i), y - h / 3)
+  }
+
+  // bottom level
+  rect(x, y, w, 5);
+  rect(x, y + 2, w, 5);
+
+  stroke(128, 0, 0);
+  rect(x, y, buildingWidth, storyHeight);
 }
