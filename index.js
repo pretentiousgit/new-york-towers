8294b7a3b68343177170ea09cd87b4c0ef8eab28
Alex Leitch <alex.leitch@gmail.com>
2019-06-22 20:21:49 -0400

    working on better default setups

--- index.js
+++ index.js
@@ -11,11 +11,7 @@ const stories = 8;
 
 const buildingWidth = canvas[0] / 2;
 const storyHeight = (canvas[1] - 40) / stories;
-// const colWidth = 
-const storyBlock = [buildingWidth, storyHeight];
-
 const margin = (canvas[0] - buildingWidth) / 2; // center the building
-
 const buildingOrigin = [margin, canvas[1] - (storyHeight - 20)];
 
 const panelStyles = [panelPane, onePaneWindow, squarePaneWindow];
@@ -38,12 +34,24 @@ function draw() {
       buildingOrigin[1] - (storyHeight * i + 2) :
       buildingOrigin[1];
 
-    basicStory([margin, y], storyBlock, i + 1);
+    const marginLeft = margin;
+    const marginTop = y * i;
+    basicStory([marginLeft, marginTop], buildingWidth, storyHeight, i + 1);
+      
+    /* reference boxes 1 */
+    fill(0, 0, 0);
+    line(0, 0, buildingWidth * 2, 0);
+    rect(buildingWidth * 2, marginTop, -30, 5);
+    
+    fill(128, 0, 0);
+    rect(0, marginTop, 30, 5);
+    line(0, marginTop, buildingWidth * 2, marginTop);
+    /* end reference boxes */
+
   }
-  // basicStory([margin, y], storyBlock, 1);
 }
 
-function basicStory(o = buildingOrigin, size = storyBlock, scaleWeight) {
+function basicStory(o = buildingOrigin, w=buildingWidth, h=storyHeight, scaleWeight) {
   stroke(0, 0, 0);
   noFill();
   const window = getRandomIntInclusive(0, panelStyles.length);
@@ -53,8 +61,10 @@ function basicStory(o = buildingOrigin, size = storyBlock, scaleWeight) {
 
   for (let i = 0; i < 2; i += 1) {
     fireEscapeLayer(
-      buildingWidth / 2,
-      storyHeight
+      w,
+      h,
+      o[0],
+      o[1]
     );
   }
   // rotate(-60);
@@ -75,36 +85,36 @@ function basicStory(o = buildingOrigin, size = storyBlock, scaleWeight) {
   // baseBlock;
 }
 
-function fireEscapeLayer(w = buildingWidth / 2, h = storyHeight, x = buildingWidth / 2, y = storyHeight * 2.5) {
+function fireEscapeLayer(w = buildingWidth / 2, h = storyHeight, x = buildingWidth / 2, bottomOfStory = storyHeight) {
   stroke(0, 0, 0);
   noFill();
   const railStart = x + w / 5;
   const railEnd = w + x - w / 5;
-  line(railStart, y, railEnd, h)
+  line(railStart, bottomOfStory, railEnd, h)
 
   stroke(0, 124, 124);
-  line(railStart + 10, y, railEnd, h + 10)
+  line(railStart + 10, bottomOfStory, railEnd, h + 10)
 
   stroke(0, 124, 0);
-  line(railStart, y - 15, railEnd, h - 15) // center line
+  line(railStart, bottomOfStory - 15, railEnd, h - 15) // center line
   stroke(0, 0, 0);
 
-  line(railStart, y - 22, railEnd, h - 22)
+  line(railStart, bottomOfStory - 22, railEnd, h - 22)
 
-  fill(255,255,255);
-  circle(railStart, y - 20, 5)
+  fill(255, 255, 255);
+  circle(railStart, bottomOfStory - 20, 5)
   noFill();
-  circle(railStart, y - 20, 9)
-  fill(255,255,255);
+  circle(railStart, bottomOfStory - 20, 9)
+  fill(255, 255, 255);
   circle(railEnd, h - 18, 9)
   circle(railEnd, h - 18, 5)
   noFill();
-  
-  rect(x, y - h / 3, w, h / 3);
+
+  rect(x, bottomOfStory - h / 3, w, h / 3);
 
   const railSupports = 9;
   for (let i = 1; i <= railSupports; i += 1) {
-    const rise = (x) => y + 15 - ((h + 56) / railSupports) * x;
+    const rise = (x) => bottomOfStory + 15 - ((h + 56) / railSupports) * x;
     const run = railStart + ((railEnd - railStart) / railSupports * i);
     line(run, rise(i), run, rise(i) - h / 3)
     rect(run - 7, rise(i) - 12, 7, 3);
@@ -112,13 +122,13 @@ function fireEscapeLayer(w = buildingWidth / 2, h = storyHeight, x = buildingWid
 
   const numberOfSupports = 18;
   for (let i = 0; i < numberOfSupports; i += 1) {
-    line(x + (w / numberOfSupports * i), y, x + (w / numberOfSupports * i), y - h / 3)
+    line(x + (w / numberOfSupports * i), bottomOfStory, x + (w / numberOfSupports * i), bottomOfStory - h / 3)
   }
 
   // bottom level
-  rect(x, y, w, 5);
-  rect(x, y + 2, w, 5);
+  rect(x, bottomOfStory, w, 5);
+  rect(x, bottomOfStory + 2, w, 5);
 
   stroke(128, 0, 0);
-  rect(x, y, buildingWidth, storyHeight);
+  rect(x, bottomOfStory, buildingWidth, storyHeight);
 }
