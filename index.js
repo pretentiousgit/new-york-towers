aefbc6cf3acad99248c033340d503f35ae78e068
Alex Leitch <alex.leitch@gmail.com>
2019-06-23 15:22:18 -0400

    rails are in place

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function setup() {
 
 function draw() {
   background(255); // Set the background to black
- 
+
   /* Test rectangle for full canvas size onscreen */
   // fill(0, 124, 124);
   // rect(0, 0, canvas[0],canvas[1]);
@@ -39,20 +39,20 @@ function draw() {
   for (let i = 0; i < stories; i += 1) {
     const marginLeft = margin;
     const marginTop = y * i;
-    
+
     /* reference boxes 1 */
-    const lineY =  storyHeight * i;
+    const lineY = storyHeight * i;
 
     fill(0, 0, 0);
     line(0, lineY, buildingWidth * 2, lineY);
     rect(0, lineY, 30, 5);
-    
+
     fill(128, 0, 0);
     rect(buildingWidth * 2, lineY + storyHeight, -30, -5);
     /* end reference boxes */
 
     basicStory(
-      buildingWidth/2,
+      buildingWidth / 2,
       storyHeight,
       buildingOrigin[0],
       lineY
@@ -60,7 +60,7 @@ function draw() {
   }
 }
 
-function basicStory(w=buildingWidth, h=storyHeight, x, y,  scaleWeight) {
+function basicStory(w = buildingWidth, h = storyHeight, x, y, scaleWeight) {
   stroke(0, 0, 0);
   noFill();
   const window = getRandomIntInclusive(0, panelStyles.length);
@@ -68,8 +68,8 @@ function basicStory(w=buildingWidth, h=storyHeight, x, y,  scaleWeight) {
   // things should be symmetric
   // they can also be multiply-defined
 
-  
-  fireEscapeLayer( w, h, x, y );
+
+  fireEscapeLayer(w, h, x, y);
   // rotate(-60);
   // symmetricWindowSeries(
   //   getRandomIntInclusive(1, 3),
@@ -92,38 +92,42 @@ function basicStory(w=buildingWidth, h=storyHeight, x, y,  scaleWeight) {
 function fireEscapeLayer(w = buildingWidth / 2, h = storyHeight, x = buildingWidth / 2, y = storyHeight) {
   stroke(0, 0, 0);
   noFill();
-  const levelBottom = y+h;
-  const railStart = x + w / 5;
-  const railEnd = w + x - w / 5;
-  
+
+  const levelBottom = y + h;
+  const levelTop = y;
+  const railTop = y - h / 3;
+  const railStart = x + w / 4;
+  const railEnd = w + x - w / 4;
+
   // Platform
-  
-  rect(x-2, levelBottom - h/3, w +4, 2);
+
+  rect(x - 2, levelBottom - h / 3, w + 4, 2);
   const numberOfSupports = 18;
   for (let i = 0; i <= numberOfSupports; i += 1) {
     line(x + (w / numberOfSupports * i), levelBottom, x + (w / numberOfSupports * i), levelBottom - h / 3)
   }
 
   // bottom level
-  rect(x, levelBottom-5, w, 5);
-  rect(x, levelBottom -7, w, 5);
+  rect(x, levelBottom - 5, w, 5);
+  rect(x, levelBottom - 7, w, 5);
 
-  
+
   /* // Rails */
-  // const railSupports = 9;
-  // for (let i = 1; i <= railSupports; i += 1) {
-  //   const rise = (x) => y + 15 - ((h + 56) / railSupports) * x;
-  //   const run = railStart + ((railEnd - railStart) / railSupports * i);
-  //   // line(run, rise(i), run, rise(i) - h / 3)
-  //   // rect(run - 7, rise(i) - 12, 7, 3);
-  // }
-  // line(railStart, y, railEnd, h)
+  const railSupports = 9;
+  for (let i = 1; i <= railSupports; i += 1) {
+    const rise = levelBottom + 15 - ((h + 15) / railSupports) * i;
+    const run = railStart + ((railEnd - railStart) / railSupports * i);
 
-  // stroke(0, 124, 124);
-  // line(railStart + 10, y, railEnd, h + 10)
+    line(run, rise, run, rise - h / 3)
+    rect(run - 7, rise - 12, 7, 3);
+  }
 
-  // stroke(0, 124, 0);
-  // line(railStart, y - 15, railEnd, h - 15) // center line
+  stroke(255, 0, 0);
+  line(railStart, levelBottom, railEnd - 10, levelTop)
+
+  stroke(0, 124, 124);
+  line(railStart + 10, levelBottom, railEnd, levelTop)
+
   // stroke(0, 0, 0);
 
   // line(railStart, y - 22, railEnd, h - 22)
@@ -136,5 +140,5 @@ function fireEscapeLayer(w = buildingWidth / 2, h = storyHeight, x = buildingWid
   // fill(255, 255, 255);
   // circle(railEnd, h - 18, 9)
   // circle(railEnd, h - 18, 5)
-  
+
 }
