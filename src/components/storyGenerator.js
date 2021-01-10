e72e400c2aff6f7f2de69d8f70053c290c9525ad
Alex Leitch <aleitch1@umd.edu>
2021-01-10 18:23:20 -0500

    Create storyGenerator.js

---
+++ src/components/storyGenerator.js
@@ -0,0 +1,34 @@
+import { getRandomIntInclusive } from './utils';
+
+function storyGenerator(buildingNumbers, sk) {
+  const {
+    windowStyle,
+    stories,
+    storyHeight
+  } = buildingNumbers;
+
+  const storyArray = stories.map((m, i) => {
+    // ground floors are different than normal building stories
+    // they do not have fire escapes
+    // and have wildly unique windows
+    // this is supposed to set if we're at the BOTTOM of the building, which is ironically the _largest_ number;
+    const groundFloor = Boolean(i === stories.length);
+    const height = (groundFloor) ? storyHeight * 1.5 : storyHeight;
+    const numberOfWindows = (!groundFloor) ? getRandomIntInclusive(2, 5) : null;
+    const windows = (groundFloor) ? windowStyle : null;
+    const lineY = height * i;
+
+    return {
+      ...buildingNumbers,
+      groundFloor,
+      height,
+      numberOfWindows,
+      windows,
+      lineY
+    };
+  });
+
+  return storyArray;
+}
+
+export default storyGenerator;
