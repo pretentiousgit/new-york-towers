5fbc3b5b0f581ca3821ac9d13c915bb6b2a47cba
Alex Leitch <aleitch1@umd.edu>
2021-01-24 18:45:52 -0500

    Clean up webpack so it serves bundle in dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
   },
   module: {
     rules: [
