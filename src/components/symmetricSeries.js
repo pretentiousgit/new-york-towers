01bfd38faa093596d31605738e8074c608883fba
pretentiousgit <alex.leitch@gmail.com>
2021-02-07 16:07:59 -0500

    Remove vertical pane ref and re-add pairs

--- src/components/symmetricSeries.js
+++ src/components/symmetricSeries.js
@@ -1,5 +1,4 @@
 import { getRandomIntInclusive, isEven } from './utils';
-import { verticalPaneDef } from './verticalPane';
 
 /*
     numberOfWindows: numberOfWindows,
@@ -17,8 +16,6 @@ function symmetricWindowSeries(config) {
   const ac = getRandomIntInclusive(0, numberOfWindows);
   const acCount = 0;
 
-  // const panelsInWindows = getRandomIntInclusive(1, 3);
-  const panelsInWindows = 1;
   const pairs = (!isEven(numberOfWindows))
     ? (numberOfWindows - 1) / 2
     : numberOfWindows / 2;
@@ -26,25 +23,24 @@ function symmetricWindowSeries(config) {
   const centerLine = buildingX + buildingWidth / 2;
   const centeredX = (buildingX + buildingWidth / 2) - windowWidth / 2;
 
-  const verticalPaneConfig = {
-    p5Sketch, panelsInWindows, drawWindowFn, x: centeredX, y, w:windowWidth, ac
+  const windowConfig = {
+    p5Sketch, drawWindowFn, x: centeredX, y, w:windowWidth, ac
   }
-  console.log('check position', verticalPaneConfig.x, verticalPaneConfig.y)
-  console.log('check width', verticalPaneConfig.w)
+  console.log('check position', windowConfig.x, windowConfig.y)
+  console.log('check width', windowConfig.w)
   // debugger;
-  drawWindowFn(verticalPaneConfig);
+  drawWindowFn(windowConfig);
   // debugger;
-  // for (let i = 0; i < pairs; i += 1) {
-  //   const interval = buildingWidth / numberOfWindows;
+  for (let i = 0; i < pairs; i += 1) {
+    const interval = buildingWidth / numberOfWindows;
 
-  //   const x1 = (centerLine - buildingWidth / 2 + (interval * i + 1)) + ((interval - windowWidth) / 2);
-  //   const x2 = (centerLine + (interval * i) + (interval - (windowWidth / 2)));
-  //   const xL = x1;
-  //   const xR = x2;
-
-  //   verticalPaneDef(panelsInWindows, drawWindowFn, xL, y, windowWidth, ac);
-  //   verticalPaneDef(panelsInWindows, drawWindowFn, xR, y, windowWidth, ac);
-  // }
+    const x1 = (centerLine - buildingWidth / 2 + (interval * i + 1)) + ((interval - windowWidth) / 2);
+    const x2 = (centerLine + (interval * i) + (interval - (windowWidth / 2)));
+    const xL = x1;
+    const xR = x2;
+      drawWindowFn({...windowConfig, x: xR});
+      drawWindowFn({...windowConfig, x: xL});
+  }
 }
 
 export { symmetricWindowSeries };
