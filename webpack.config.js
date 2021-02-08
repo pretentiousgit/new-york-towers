cc2bb731e1adb34fadfcd729d943ffdd74d4b5d7
pretentiousgit <alex.leitch@gmail.com>
2021-02-08 10:04:55 -0500

    Add babel to allow async/await keywords

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/main.js',
+  entry: ['babel-polyfill', './src/main.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
@@ -15,7 +15,7 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /(node_modules)/,
+        // exclude: /(node_modules)/,
         use: {
           loader: 'babel-loader',
           options: {
