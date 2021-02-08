33e7922eabfe371609bfc1d125b2b1809036064e
pretentiousgit <alex.leitch@gmail.com>
2021-02-08 10:23:53 -0500

    use limited use of popular fonts

--- src/library/fontSwapper.js
+++ src/library/fontSwapper.js
@@ -1,13 +1,54 @@
 import WebFont from 'webfontloader';
+import { getRandomIntInclusive } from './utils';
 
 const API_KEY = 'AIzaSyCxDDCT8nUfp2FV9y0wKL4gOoq5FE3iX3s';
+
+const popular2020 = [
+  'Alegreya',
+  'B612',
+  'Muli',
+  'Titillium Web',
+  'Varela',
+  'Vollkorn',
+  'IBM Plex',
+  'Crimson Text',
+  'Cairo',
+  'BioRhyme',
+  'Karla',
+  'Lora',
+  'Frank Ruhl Libre',
+  'Playfair Display',
+  'Archivo',
+  'Spectral',
+  'Fjalla One',
+  'Roboto',
+  'Montserrat',
+  'Rubik',
+  'Source Sans',
+  'Cardo',
+  'Cormorant',
+  'Work Sans',
+  'Rakkas',
+  'Concert One',
+  'Yatra One',
+  'Arvo',
+  'Lato',
+  'Abril FatFace',
+  'Ubuntu',
+  'PT Serif',
+  'Old Standard TT',
+  'Oswald'
+];
 
 async function loadFontsList() {
   try {
     const result = await fetch(`https://www.googleapis.com/webfonts/v1/webfonts?key=${API_KEY}`);
     const data = await result.json();
     console.log('loaded google fonts list: ', data.items.length);
-    return data.items;
+    console.log('loaded google fonts list: ', data.items[2]);
+    const popular = data.items.filter((f) => popular2020.includes(f.family));
+    console.log('popular', popular);
+    return popular;
   } catch (error) {
     console.log('loadFontsList', error, error.message);
     return error;
